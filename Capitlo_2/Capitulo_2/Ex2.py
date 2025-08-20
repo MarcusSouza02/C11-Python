45504000eafa34cd2465fc49c17e36d3cd84fa23
Marcus Vinicius <111466820+MarcusSouza02@users.noreply.github.com>
2025-08-20 20:48:29 -0300

    Delete Capitlo_2/Capitulo_2 directory

--- Capitlo_2/Capitulo_2/Ex2.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Escolha um número: "))
-inicio = int(input("Digite o início do intervalo: "))
-fim = int(input("Digite o fim do intervalo: "))
-
-for i in range(inicio, fim + 1):
-    print(f"{numero} x {i} = {numero * i}")
--- Capitlo_2/Capitulo_2/Ex6.py
+++
@@ -1,8 +0,0 @@
-import math
-
-numero = float(input("Digite um número decimal: "))
-
-print("Raiz quadrada:", math.sqrt(numero))
-print("Teto:", math.ceil(numero))
-print("Chão:", math.floor(numero))
-print("Parte inteira:", int(numero))
