23d8dbd255d4891c3ce8b8045394a3479411c383
Marcus Vinicius <111466820+MarcusSouza02@users.noreply.github.com>
2025-08-20 20:48:53 -0300

    Add files via upload

---
+++ Capitulo_2/Ex1.py
@@ -0,0 +1,14 @@
+nome = "Marcus Vinicius Faria de Souza"
+
+maiuscula = nome.upper()
+minuscula = nome.lower()
+
+letras = nome.replace(" ", "")
+qtd = len(letras)
+
+sobrenome = nome.replace("de Souza", "do Inatel")
+
+print(maiuscula)
+print(minuscula)
+print(qtd)
+print(sobrenome)
---
+++ Capitulo_2/Ex6.py
@@ -0,0 +1,8 @@
+import math
+
+numero = float(input("Digite um número decimal: "))
+
+print("Raiz quadrada:", math.sqrt(numero))
+print("Teto:", math.ceil(numero))
+print("Chão:", math.floor(numero))
+print("Parte inteira:", int(numero))
